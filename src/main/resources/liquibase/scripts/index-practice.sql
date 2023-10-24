96df70519fd6fabd7c91c8d01eefba9ada659b80
roma <rommur5@mail.ru>
2023-10-24 18:04:58 +0300

    INDEX SQL DB commit

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
---chengeset roma:1
+--changeset roma:1
 CREATE INDEX student_name_index ON student (name);
 
---chengeset roma:2
+--changeset roma:2
 CREATE INDEX faculty_nc_idx ON faculty (name, color);
